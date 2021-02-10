f9e03a33783a3e47e5b48563e98860de7be70934
Maick <eee9@i.ua>
2021-02-10 11:51:54 +0200

    shell files updated, Hot Reload checked.

--- App.tsx
+++ App.tsx
@@ -1,25 +1,56 @@
-/* eslint-disable react-native/no-inline-styles */
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text, Button, BackHandler, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  BackHandler,
+  Platform,
+  StyleSheet,
+} from 'react-native';
 
-export const andAPIlvl = (): string => Platform.OS === 'android' ? Platform.Version.toString() : '';
+// Note. Using here export breaks quick Hot Reload
+const andAPIlvl = (): string =>
+  Platform.OS === 'android' ? Platform.Version.toString() : '';
+
+const appVer = 'r07, L2A';
 
 const App = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'cyan', alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: 'black', fontSize: 22}}>
-        r02, L2A
-      </Text>
-      <Text style={{color: 'blue', fontSize: 18}}>
-        Hi, Simplest React Native app.
-      </Text>
-      <Text style={{color: 'red', fontSize: 14}}>
-        (TypeScript, API: {andAPIlvl()})
-      </Text>
+    <View style={styles.MainView}>
+      <Text style={styles.Text1}>{appVer}</Text>
+      <Text style={styles.Text2}>Simple React Native app.</Text>
+      <Text style={styles.Text3}>(TypeScript, API: {andAPIlvl()})</Text>
       <Text> </Text>
-      <Button title="  Exit  " onPress={() => { BackHandler.exitApp();}} />
+      <Button
+        title="  Exit  "
+        onPress={() => {
+          BackHandler.exitApp();
+        }}
+      />
     </View>
   );
 };
 export default App;
+
+const styles = StyleSheet.create({
+  MainView: {
+    flex: 1,
+    backgroundColor: 'cyan',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Text1: {
+    color: 'black',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  Text2: {
+    color: 'blue',
+    fontSize: 18,
+  },
+  Text3: {
+    color: 'red',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+});
---
+++ README.md
@@ -0,0 +1,19 @@
+# simple-rn-and
+
+Just a simple React Native boilerplate, L2A.
+Typescript, Android only, Hermes is on, apk split by abi.
+Latest npm packages (nl-*.txt files).
+
+Note for distros like MX Linux (Debian like distros, unchecked).
+Insted of:
+  yarn start
+  yarn android
+may run:
+  ss/sss
+
+Result apk:
+ -app-arm64-v8a-release.apk  - 5146173
+ -app-armeabi-v7a-elease.apk - 4801716
+ -app-x86_64-releae.apk      - 5426401
+ -app-x86-release.pk         - 5530653
+ -output-metadata.son        -    1202
